d0d8bef72883bafbad29cfbe2e7b4002c8a0db88
KOUA ELYSEE KEVIN <91912825+elydev01@users.noreply.github.com>
2021-11-16 16:43:41 +0000

    Update home.py

--- controllers/home.py
+++ controllers/home.py
@@ -1,10 +1,18 @@
 from kivy.lang import Builder
+from kivy.metrics import dp
+from kivy import properties as p
+from kivy.animation import Animation
+
 from kivymd.app import MDApp as App
 from kivymd.uix.screen import MDScreen
 
 
 class HomeMainScreen(MDScreen):
-    pass
+    bg_pos = p.NumericProperty(0)
+    
+    def toggle_bg_pos(self):
+        bg_pos = 0 if self.bg_pos > 0 else dp(self.height/2)
+        Animation(bg_pos=bg_pos).start(self)
 
 
 with open('views/home.kv', encoding='utf-8') as f:
